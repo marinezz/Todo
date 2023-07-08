637be13f222ef1e4708309fdb10106949936b2f6
marine <qazsw153@163.com>
2023-07-08 16:17:18 +0800

    rest风格开发

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func sayHello(c *gin.Context) {
 	c.JSON(200, gin.H{ // 返回一个Json的数据，状态码为200
@@ -13,6 +16,31 @@ func main() {
 
 	// 指定用户使用get请求访问hello，执行sayHello函数
 	r.GET("hello", sayHello)
+
+	// rest风格请求
+	r.GET("book", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "Get",
+		})
+	})
+
+	r.POST("book", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "Post",
+		})
+	})
+
+	r.DELETE("book", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "Delete",
+		})
+	})
+
+	r.PUT("book", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "Put",
+		})
+	})
 
 	// 启动服务,括号中可以指定端口
 	r.Run()
