0ea7bb997d93c065f165a3bcbe1086b19c338325
marine <qazsw153@163.com>
2023-07-08 15:56:53 +0800

    gin框架快速入门

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func main() {
-	r := gin.Default()
+func sayHello(c *gin.Context) {
+	c.JSON(200, gin.H{ // 返回一个Json的数据，状态码为200
+		"message": "hello golang",
+	})
+}
 
-	r.
+func main() {
+	r := gin.Default() // 返回默认的路由引擎
+
+	// 指定用户使用get请求访问hello，执行sayHello函数
+	r.GET("hello", sayHello)
+
+	// 启动服务,括号中可以指定端口
+	r.Run()
 }
